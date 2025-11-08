9afd7953d91a7574d14eea3b2830e6d1446514f8
Rhuviyn <rhuviyn@mail.ru>
2025-11-08 18:05:04 +0300

    Базовая структура данных

---
+++ web/converter.js
@@ -0,0 +1,168 @@
+class Grammar {
+    /** @type {String[]} */
+    cellTypes
+    /** @type {Map<String, Pattern>} */
+    patterns
+    /** @type {String} */
+    rootName
+
+    constructor(cellTypes, patterns, rootName) {
+        this.cellTypes = cellTypes;
+        this.patterns = patterns;
+        this.rootName = rootName;
+    }
+}
+
+class CellOffset {
+    /** @type {number}  */
+    left
+    /** @type {number} */
+    top 
+    /** @type {number} */
+    right 
+    /** @type {number} */
+    bottom 
+
+    constructor(left, top, right, bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+}
+
+class Location {
+    /** @type {CellOffset} */
+    padding
+    /** @type {CellOffset} */
+    margin
+
+    constructor(padding, margin) {
+        this.padding = padding;
+        this.margin = margin;
+    }
+}
+
+class Range {
+    /** @type {number} */
+    #begin
+    /** @type {number} */
+    #end
+
+    constructor(begin, end) {
+        if (begin > end) {
+            throw new Error('Конец диапазона не может быть больше начала');
+        }
+        this.#begin = begin;
+        this.#end = end;
+    }
+
+    getBegin() {
+        return this.#begin;
+    }
+
+    getEnd() {
+        return this.#end;
+    }
+
+    setBegin(value) {
+        if (this.getEnd() < value) {
+            return;
+        }
+        this.#begin = value;
+    }
+
+    setEnd(value) {
+        if (this.getBegin > value) {
+            return;
+        }
+        this.#end = value;
+    }
+}
+
+class Component {
+    /** @type {String} */
+    name 
+    /** @type {Pattern} */
+    pattern 
+    /** @type {Location} */
+    location
+    /** @type {boolean} */
+    optional
+    /** @type {boolean} */
+    inner
+
+    constructor(name, pattern, location, optional, inner) {
+        this.name = name;
+        this.pattern = pattern;
+        this.location = location;
+        this.optional = optional;
+        this.inner = inner;
+    }
+}
+
+class Pattern {
+    /** @type {String} */
+    name
+    /** @type {"CELL" | "AREA" | "ARRAY"}  */
+    kind
+    /** @type {String} */
+    desc
+    /** @type {Range} */
+    countInDoc
+    /** @type {Range} */
+    width
+    /** @type {Range} */
+    height
+    /** @type {boolean} */
+    isRoot
+
+    constructor(name, kind, desc, countInDoc, width, height, isRoot) {
+        this.name = name;
+        this.kind = kind;
+        this.desc = desc;
+        this.countInDoc = countInDoc;
+        this.width = width;
+        this.height = height;
+        this.isRoot = isRoot;
+    }
+}
+
+class Cell extends Pattern {
+    /** @type {String} */
+    contentType
+
+    constructor(name, kind, desc, count, width, height, isRoot, extend, contentType) {
+        super(name, kind, desc, count, width, height, isRoot, extend);
+        this.contentType = contentType;
+    }
+}
+
+class Array extends Pattern {
+    /** @type {"ROW" | "COL" | "AREA"} */
+    direction
+    /** @type {Pattern} */
+    pattern
+    /** @type {Range} */
+    gap
+    /** @type {Range} */
+    itemCount
+
+    constructor(name, kind, desc, count, width, height, isRoot, extend, direction, pattern, gap, itemCount) {
+        super(name, kind, desc, count, width, height, isRoot, extend);
+        this.direction = direction;
+        this.pattern = pattern;
+        this.gap = gap; 
+        this.itemCount = itemCount;
+    }
+}
+
+class Area extends Pattern {
+    /** @type {Component[]} */
+    components
+
+    constructor(components) {
+        this.components = components;
+    }
+}
+
