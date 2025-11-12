c2131a34c32e33037109a7e66b6c8c6134e0de5e
RedstoneFX <zatmonhp@yandex.ru>
2025-11-12 21:27:59 +0300

    Реализован метод UI.resetUI()

--- web/UI.js
+++ web/UI.js
@@ -1,6 +1,57 @@
 class UI {
 
     static resetUI() {
+        this.clearBrowser();
+        this.setPatternParamsEnabled(false);
+        this.setComponentParamsEnabled(false);
+    }
+
+    static clearBrowser() {
+        for (let i = this.browser.children.length - 1; i >= 0; --i)
+            this.browser.children[i].remove();
+    }
+
+    /**
+     * Устанавливает секцию параметров видимой/невидимой
+     * @param {boolean} isEnabled 
+     */
+    static setPatternParamsEnabled(isEnabled) {
+        this.patternParams.hidden = !isEnabled;
+        if (!isEnabled) {
+            this.cellParams.hidden = true;
+            this.arrayParams.hidden = true;
+        } else {
+            this.setComponentParamsEnabled(false);
+        }
+    }
+
+    /**
+ * Устанавливает секцию параметров видимой/невидимой
+ * @param {boolean} isEnabled 
+ */
+    static setCellParamsEnabled(isEnabled) {
+        this.patternParams.hidden = !isEnabled;
+        this.cellParams.hidden = !isEnabled;
+        if (isEnabled) this.arrayParams.hidden = true;
+    }
+
+    /**
+ * Устанавливает секцию параметров видимой/невидимой
+ * @param {boolean} isEnabled 
+ */
+    static setArrayParamsEnabled(isEnabled) {
+        this.patternParams.hidden = !isEnabled;
+        this.arrayParams.hidden = !isEnabled;
+        if (isEnabled) this.cellParams.hidden = true;
+    }
+
+    /**
+ * Устанавливает секцию параметров видимой/невидимой
+ * @param {boolean} isEnabled 
+ */
+    static setComponentParamsEnabled(isEnabled) {
+        this.componentParams.hidden = !isEnabled;
+        if (isEnabled) this.setPatternParamsEnabled(false);
     }
 
     /**
