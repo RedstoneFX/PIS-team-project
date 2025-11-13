c592011fd3f3c21816602a52ac7f1146561363ad
Rhuviyn <rhuviyn@mail.ru>
2025-11-13 15:03:13 +0300

    Сохранение данных в LocalStorage

--- web/engine.js
+++ web/engine.js
@@ -1,6 +1,16 @@
 
 /// <reference path="converter.js" />
 
+function saveToLocalStorage() {
+    localStorage.setItem("GrammarData", JSON.stringify(Grammar.toYamlObject()));
+}
+
+function loadFromLocalStorage() {
+    const savedData = localStorage.getItem("GrammarData");
+    if (savedData) {
+        Grammar.parse(JSON.parse(savedData));
+    }
+}
 
 function onPageLoaded() {
     let testData = YAML.load(request("cnf/grammar_root.yml"));
@@ -8,12 +18,15 @@ function onPageLoaded() {
 
     try {
         Grammar.parse(testData);
+        loadFromLocalStorage();
         UI.loadFromGrammar();
     } catch (e) {
         UI.resetUI();
         alert(e.message);
         throw e;
     }
+
+    setInterval(saveToLocalStorage, 300000);
 }
 
 
